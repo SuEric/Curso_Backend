fb8f9f26f4224b228eab903d985ade82dee9ae1d
Eric García Pérez <suericgp@gmail.com>
2014-11-25 13:06:57 -0600

    Models y Views
    Se hizo makemigrations y nuevas cosas en los models y views

--- project/app/views.py
+++ project/app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render_to_response
 from datetime import datetime
 
 # El shortcut render_to_response equivale a todo esto
-from django.http import HttpResponse
+"""from django.http import HttpResponse
 from django.template import Context
-from django.template.loader import get_template
+from django.template.loader import get_template"""
 
 # Create your views here.
 def hora_actual(request):
