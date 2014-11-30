1160b92e4dad3c93d156aab9bf0ae88d8aae9b27
Eric García Pérez <suericgp@gmail.com>
2014-11-30 09:27:57 -0600

    Casi finalizando clase 2
    falta poco

--- project/app/views.py
+++ project/app/views.py
@@ -1,10 +1,71 @@
 from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404 
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.template.context import RequestContext
 from datetime import datetime
+from forms import *
+from models import *
 
+
+"""
 # El shortcut render_to_response equivale a todo esto
-"""from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import Context
-from django.template.loader import get_template"""
+from django.template.loader import get_template
+"""
+
+def home(request):
+	categorias = Categoria.objects.all()
+	enlaces = Enlace.objects.all()
+	template = "index.html"
+
+	#diccionario = {"categorias" : categorias, "enlaces" : enlaces}
+	return render_to_response(template, locals())
+
+def categoria(request, id_categoria):
+	categorias = Categoria.objects.all()
+
+	#sino existe la categoria
+	cat = get_object_or_404(Categoria, pk = id_categoria)
+
+	#este no evalua si existe o no
+	#cat = Categoria.objects.get(pk = id_categoria)
+	
+	enlaces = Enlace.objects.filter(categoria = cat)
+	template = "index.html"
+
+	return render_to_response(template, locals())
+
+@login_required
+def add(request):
+	if request.method == "POST":
+		form = EnlaceForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect("/")
+	else:
+		form = EnlaceForm()
+
+	template = "form.html"
+	return render_to_response(template, 
+					context_instance = RequestContext(request, locals()))
+
+@login_required
+def plus(request, id_enlace):
+	enlace = Enlace.objects.get(pk = id_enlace)
+	enlace.votos = enlace.votos + 1
+	enlace.save()
+
+	return HttpResponseRedirect("/")
+
+@login_required
+def minus(request, id_enlace):
+	enlace = Enlace.objects.get(pk = id_enlace)
+	enlace.votos = enlace.votos - 1
+	enlace.save()
+
+	return HttpResponseRedirect("/")
 
 # Create your views here.
 def hora_actual(request):
--- project/project/urls.py
+++ project/project/urls.py
@@ -3,7 +3,14 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'app.views.hora_actual', name='hora_actual'),
+    #url(r'^$', 'app.views.hora_actual', name='hora_actual'),
+
+    # home de puls4
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^categoria/(\d+)$', 'app.views.categoria', name='categoria'),
+   	url(r'^plus/(\d+)$', 'app.views.plus', name='plus'),
+   	url(r'^minus/(\d+)$', 'app.views.minus', name='minus'),
+   	url(r'add/$', 'app.views.add', name='add'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
